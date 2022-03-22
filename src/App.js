fd1bcccbc95fafd3886eaeddd520a57f44bf1d6d
Benndip <obentabiayuk1@gmail.com>
2022-03-22 23:38:45 +0100

    Adding Button that adds todoItems to array when clicked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { v4 as uuid } from 'uuid';
+
 
 function App() {
+
+  const [todoItem, setTodoItem] = useState('');
+  const [todoItems, setTodoItems] = useState([]);
+
+  const onChangeHandler = (event) => {
+    setTodoItem(event.target.value);
+  }
+
+  const addTodoItem = () => {
+    let todo_id = uuid();
+    setTodoItems([...todoItems, { id: todo_id, title: todoItem, date: new Date().toUTCString(), done: false }]);
+    console.log(todoItems);
+  }
+
   return (
     <div className="App">
       <h1>Welcome To <span className="record-it-text">Record it</span></h1>
       <div className="containers">
         <div className="left-container">
           <h2>Add a new task</h2>
-          <input type="text" className="input" placeholder="Enter your todo..."/>
-          <button className="add-task-button">Add Task</button>
+          <input onChange={onChangeHandler} name="todoItem" type="text" className="input" value={todoItem} placeholder="Enter your todo..."/>
+          <button onClick={addTodoItem} className="add-task-button">Add Task</button>
         </div>
         <div className="right-container">
           <span className="no-task-text">You have no tasks today 😀</span>
